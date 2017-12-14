e0d548a61edffb37cc4f08dde5a394fa977c375a
William Dolorito <wdolorito@gmail.com>
2017-12-13 21:46:06 -0800

    Makefile might be done!

--- depend.sh
+++ depend.sh
@@ -1,14 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 DIR="$1"
 shift 1
 case "$DIR" in
 "" | ".")
-g++ -MM -MG "$@" |
+clang++ -MM -MG "$@" |
 sed -e ’s@ˆ\(.*\)\.o:@\1.d \1.o:@’
 ;;
 *)
-g++ -MM -MG "$@" |
-sed -e "s@ˆ\(.*\)\.o:@$DIR/\1.d \
-$DIR/\1.o:@"
+clang++ -MM -MG "$@" |
+sed -e "s@ˆ\(.*\)\.o:@$DIR/\1.d $DIR/\1.o:@"
 ;;
 esac
---
+++ src/module.mk
@@ -0,0 +1,1 @@
+SRC += src/Minesweeper.cxx
