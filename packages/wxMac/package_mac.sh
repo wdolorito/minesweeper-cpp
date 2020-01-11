f7ff783f8579215f0fd1de614dbadebbcc150c18
William Dolorito <wdolorito@gmail.com>
2020-01-11 09:29:59 -0800

    package_mac.sh:   copies dynamic libraries from local install to bundle   patches bundle executable to point to bundled dylibs

--- packages/wxMac/package_mac.sh
+++ packages/wxMac/package_mac.sh
@@ -19,12 +19,12 @@ fi
 WXLIBS=`otool -L $BINDIR/minesweeper | grep libwx | cut -d " " -f 1 | cut -d $'\t' -f 2`
 APPBINDIR=./$APP.app/Contents/MacOS
 APPRSRCDIR=./$APP.app/Contents/Resources
-# LIBDEFDIR=`wx-config --libs | cut -d " " -f 1 | cut -d "L" -f 2`
+LIBDEFDIR=`wx-config --libs | cut -d " " -f 1 | cut -d "L" -f 2`
 FRAMEDIR=./$APP.app/Contents/Frameworks
 
 mkdir -p $APPBINDIR
 mkdir -p $APPRSRCDIR
-# mkdir -p $FRAMEDIR
+mkdir -p $FRAMEDIR
 
 echo "Copying bundle Info"
 cp Info.plist PkgInfo ./$APP.app/Contents
@@ -36,38 +36,32 @@ cp $ICON $APPRSRCDIR
 echo "Copying executable"
 cp $BINDIR/$EXE $APPBINDIR
 
-# echo "Copying dynamic libraries"
-# for file in $WXLIBS
-# do
-# 	cp $file $FRAMEDIR
-# done
-#
-# echo "Patching executable"
-# for libfile in $WXLIBS
-# do
-# 	install_name_tool -change $LIBDEFDIR/$(basename $libfile)  "@executable_path/../Frameworks/$(basename $libfile)" $APPBINDIR/$EXE
-# done
-#
+echo "Copying dynamic libraries"
+for file in $WXLIBS
+do
+	cp $file $FRAMEDIR
+done
+
+echo "Patching executable"
+for libfile in $WXLIBS
+do
+	install_name_tool -change $LIBDEFDIR/$(basename $libfile)  "@executable_path/../Frameworks/$(basename $libfile)" $APPBINDIR/$EXE
+done
+
 # echo "Patching dylibs"
 # for libfile in `ls $FRAMEDIR`
 # do
-# 	echo -e "\tPatching $libfile"
-# 	DYLIBS=`otool -L $FRAMEDIR/$libfile | grep -e "libwx" | grep -v -e ":" | grep -v -e "exec" | cut -d " " -f 1 | cut -d $'\t' -f 2`
-# 	for dylib in $DYLIBS
-# 	do
-# 		install_name_tool -change $dylib "@executable_path/../Frameworks/$(basename $libfile)" $FRAMEDIR/$libfile
-# 	done
-# done
+# 	echo -e "Patching $libfile"
+# 	DYNAME=`otool -D $FRAMEDIR/$libfile | grep -e "libwx" | grep -v -e ":" | cut -d " " -f 1 | cut -d $'\t' -f 2`
+# 	DYLIBS=`otool -L $FRAMEDIR/$libfile | grep -e "libwx" | grep -v -e ":" | cut -d " " -f 1 | cut -d $'\t' -f 2`
 #
-# cd $FRAMEDIR
-# for libfile in `ls *libwx*`
-# do
-# 	echo "Patching $libfile"
-# 	DYLIBS=`otool -L $libfile | grep -e "_path//" | grep -v -e ":" | cut -d " " -f 1`
-# 	echo -e "\t\t\t$DYLIBS"
+# 	# echo $DYNAME
+# 	# install_name_tool -id "../Frameworks/$(basename $DYNAME)" $DYNAME
+#
 # 	for dylib in $DYLIBS
 # 	do
-# 		echo -e "\t\t$dylib"
-# 		install_name_tool -id @executable_path/$libfile ./$libfile
+# 		echo -e "\twith @executable_path/../Frameworks/$(basename $dylib)"
+# 		install_name_tool -change $dylib "@executable_path/../Frameworks/$(basename $dylib)" $FRAMEDIR/$libfile
 # 	done
+#
 # done
