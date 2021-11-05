659872ea7a7a20116be55a7b89970db98a2fcf71
William Dolorito <wdolorito@gmail.com>
2021-11-05 15:20:43 -0700

    fixes for wxMSW xcomp on linux
    .gitignore:
      add c++ to ignore
    
    packages/wxMSW/cross_container_package.sh:
      dos2unix'ized (broken in build otherwise)
    
    packages/wMSW/release/Minesweep.i686-w64-mingw32.zip:
      binary cross compile build for i686, tested working W10 x86_64
    
    vagrant/arm-cross-build-wxWidgets.sh:
    vagrant/mingw-cross-build-wxWidgets.sh:
    vagrant/native-build-wxWidgets.sh:
      add --disable-mediactrl to configure
    
    vagrant/mingw-cross-build-project.sh:
      add additional env var to make
      fix comment for x86_64

--- vagrant/arm-cross-build-wxWidgets.sh
+++ vagrant/arm-cross-build-wxWidgets.sh
@@ -5,6 +5,6 @@ mkdir -p /root/local/src/wxWidgets-3.0.3-configure
 cd /root/local/src
 tar xf /root/project/packages/wxWidgets-3.0.3.tar.bz2
 cd wxWidgets-3.0.3-configure
-CC=clang CXX=clang++ LDFLAGS=-fuse-ld=lld-6.0 ../wxWidgets-3.0.3/configure --enable-stl --prefix=/root/local
+CC=clang CXX=clang++ LDFLAGS=-fuse-ld=lld-6.0 ../wxWidgets-3.0.3/configure --enable-stl --disable-mediactrl --prefix=/root/local
 make
 make install
--- vagrant/mingw-cross-build-project.sh
+++ vagrant/mingw-cross-build-project.sh
@@ -2,8 +2,8 @@
 
 # compile i686 project
 cd /home/vagrant/project
-PATH=/home/vagrant/local-i686/bin:$PATH make -f Makefile.mswx distclean
-PATH=/home/vagrant/local-i686/bin:$PATH make -f Makefile.mswx release
+PATH=/home/vagrant/local-i686/bin:$PATH BUILD=32 make -f Makefile.mswx distclean
+PATH=/home/vagrant/local-i686/bin:$PATH BUILD=32 make -f Makefile.mswx release
 
 # pacakage i686 project
 cd packages/wxMSW
@@ -11,9 +11,9 @@ cd packages/wxMSW
 
 # compile x86_64 project
 cd /home/vagrant/project
-PATH=/home/vagrant/local-x86_64/bin:$PATH make -f Makefile.mswx distclean
-PATH=/home/vagrant/local-x86_64/bin:$PATH make -f Makefile.mswx release
+PATH=/home/vagrant/local-x86_64/bin:$PATH BUILD=64 make -f Makefile.mswx distclean
+PATH=/home/vagrant/local-x86_64/bin:$PATH BUILD=64 make -f Makefile.mswx release
 
-# pacakage i686 project
+# pacakage x86_64 project
 cd packages/wxMSW
 ./cross_container_package.sh
